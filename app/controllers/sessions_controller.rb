578cfafb7678ba257b292df093ec18ba5915bb42
Jon Kaplon <jon.kaplon1@gmail.com>
2022-04-29 10:31:41 -0700

    commit tree 4920ea5657f2b5b99857b43979e7e08a8b1ff17c

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+    before_action :authorize, only: [:destroy]
+    
+    def create
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { errors: ["Inavlid username or password"] }, status: :unauthorized
+        end
+    end
+
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+end
+class UsersController < ApplicationController
+
+    before_action :authorize, only: [:show]
+
+    def create
+        user = User.create!(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        render json: @current_user
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation, :image_url, :bio)
+    end
+
+end
